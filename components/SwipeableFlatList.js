3afa97b7210d18b673325fd7aa13b2067986280d
SansNeal <62467577+SansNeal@users.noreply.github.com>
2020-11-21 16:46:36 +0530

    Add files via upload

---
+++ components/SwipeableFlatList.js
@@ -0,0 +1,104 @@
+import React, { Component} from 'react';
+import { View, Text, StyleSheet ,Alert,Animated, Dimensions,TouchableHighlight} from 'react-native';
+import { SwipeListView } from 'react-native-swipe-list-view';
+import { ListItem, Icon } from 'react-native-elements';
+import db from '../config';
+
+export default class SwipeableFlatList extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            allNotifications: this.props.allNotifications
+        }
+    }
+
+    updateMarkAsRead=(notification)=>{
+        db.collection("all_notifications").doc(notification.doc_id).update({
+            "notification_status"  :"read"
+        })
+    }
+
+    onSwipeValueChange =(swipeData)=>{
+        var allNotifications = this.state.allNotifications
+        const {key, value} = swipeData;
+
+        if(value< - Dimensions.get("window").width){
+            const newData = [...allNotifications];
+            const prevIndex = allNotifications.findIndex(item => item.key ===key)
+            this.updateMarkAsRead(allNotifications[prevIndex]);
+            newData.splice(prevIndex,1);
+            this.setState({
+                allNotifications: newData
+            })
+        }
+    }
+
+    renderItem=(data)=>{
+        <Animated.View>
+             <ListItem
+                leftElement={<Icon name="book" type="font-awesome" color ='#696969'/>}
+                title={data.item.book_name}
+                titleStyle={{ color: 'black', fontWeight: 'bold' }}
+                subtitle={data.item.message}
+                bottomDivider
+            />
+         </Animated.View>
+    }
+
+    renderHiddenItem=()=>{
+        <View style={styles.rowBack}>
+            <View style ={[styles.backRightBtn, styles.backRightBtnRight]}  >
+              <Text style= {styles.backTextWhite}></Text>  
+            </View>
+        </View>
+
+    }
+    render(){
+        return(
+            <View style ={styles.container}>
+                <SwipeListView
+                data={this.state.allNotifications}
+                renderItem = {this.renderItem}
+                renderHiddenItem={this.renderHiddenItem}
+                onSwipeValueChange={this.onSwipeValueChange}
+                disableRightSwipe
+                rightOpenValue={ - Dimensions.get("window").width}
+                previewRowKey={"0"}
+
+                />
+            </View>
+        )
+    }
+
+}
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'white',
+        flex: 1,
+    },
+    backTextWhite: {
+        color: '#FFF',
+        fontWeight:'bold',
+        fontSize:15
+    },
+    rowBack: {
+        alignItems: 'center',
+        backgroundColor: '#29b6f6',
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingLeft: 15,
+    },
+    backRightBtn: {
+        alignItems: 'center',
+        bottom: 0,
+        justifyContent: 'center',
+        position: 'absolute',
+        top: 0,
+        width: 100,
+    },
+    backRightBtnRight: {
+        backgroundColor: '#29b6f6',
+        right: 0,
+    },
+});
